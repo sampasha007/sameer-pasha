8c730c82719f2826743d40c7acd75a720565129e
sampasha007 <sampasha007@gmail.com>
2018-02-09 20:26:55 +0530

    UI added

--- main.py
+++ main.py
@@ -15,11 +15,47 @@
 # limitations under the License.
 #
 import webapp2
+import os
+import jinja2
+import re
+import logging
+from gaesessions import get_current_session
+from gaesessions import delete_expired_sessions
+from google.appengine.ext import db
+from google.appengine.ext import ndb
+from google.appengine.api import users
+from google.appengine.api import images
+from google.appengine.api import search
+import datetime
+import urllib2
+import urllib
+import time
+import json
+from google.appengine.api import memcache
+import cgi
+from google.appengine.ext import ndb
+import math
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+
+template_dir = os.path.join(os.path.dirname(__file__),'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+							   autoescape=True)
+
+class Handler(webapp2.RequestHandler):
+	def write(self,*a,**kw):
+		self.response.out.write(*a,**kw)
+	def render_str(self,template,**params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+	def render(self,template,**kw):
+		self.write(self.render_str(template,**kw))
+	def cache(self, key):
+		return memcache.get(key)
+
+class HomeHandler(Handler):                          
+	def get(self):     
+		self.render("index.html")
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', HomeHandler)
 ], debug=True)
